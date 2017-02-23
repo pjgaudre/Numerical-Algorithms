a876ec2e9d1cbbbeb458627af4369278bac981fe
Philippe <pjgaudreau@gmail.com>
2017-02-22 22:09:16 -0700

    finalized the Cholesky Factorization

--- Cholesky.py
+++ Cholesky.py
@@ -1,28 +1,53 @@
 import numpy as np
+from copy import copy
+import warnings
+warnings.filterwarnings('error')
 
 def Cholesky(A):
     try:
         n,m = A.shape
-        if n!=m:
-            print "Matrix must be square"
-            return
     except:
-        print "Array must be a two dimensioal"
-    for k in xrange(n):
-        A[k,k] = np.sqrt(A[k,k])
-        for i in xrange(k+1,n):
-            A[i,k] /= A[k,k]
-        for j in xrange(k+1,n):
-            for i in xrange(j,n):
-                A[i,j] -= A[i,k]*A[j,k]
-    return np.triu(A)
-np.random.seed(10)
-A = np.random.rand(n,n)
-A = (A + A.T)/2.0
-A = A.dot(A.T)+np.diag([1.0*n for i in range(len(b))])
-L = Cholesky(A)
-np.random.seed(10)
-A = np.random.rand(n,n)
-A = (A + A.T)/2.0
-A = A.dot(A.T)+np.diag([1.0*n for i in range(len(b))])
-L.T.dot(L)-A
+        print "Array must be a two dimensioal."
+        return
+    if n!=m:
+        print "Matrix must be square."
+        return
+    if not (A.transpose() == A).all():
+        print "Matrix must be symmetric."
+        return
+    try:
+        for k in xrange(n):
+            A[k,k] = np.sqrt(A[k,k])
+            for i in xrange(k+1,n):
+                A[i,k] /= A[k,k]
+            for j in xrange(k+1,n):
+                for i in xrange(j,n):
+                    A[i,j] -= A[i,k]*A[j,k]
+        return np.tril(A)
+    except:
+        print "LinAlgError: Matrix is not positive definite - Cholesky decomposition cannot be computed"
+        return
+
+def Cholesky_test():
+    n=40
+    np.random.seed(10)
+    # Two dimensional test
+    A = np.random.rand(n)
+    L = Cholesky(copy(A))
+    # Non square matrix case
+    A = np.random.rand(n,n+1)
+    L = Cholesky(copy(A))
+    # Non symmetric case
+    A = np.random.rand(n,n)
+    L = Cholesky(copy(A))
+    # When it works, does it converge?
+    A = A.dot(A.T)
+    L = Cholesky(copy(A))
+    if np.linalg.norm(L.dot(L.T)-A)<=1e-10:
+        print "Test passed"
+    else:
+        print "test failed"
+    # Matrix is not positive definite
+    A = A - np.diag([10.0]*n)
+    L = Cholesky(copy(A))
+Cholesky_test()
